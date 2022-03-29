2764dca4a4cc820180a558dbe5e357ce762ed6bb
Darkest86 <darkest86@gmail.com>
2022-03-29 21:43:47 +0500

    Delete TicTacToeREST directory

--- TicTacToeREST/tictactoegame/src/main/java/com/tictactoerest/tictactoegame/parsers/parser.java
+++
@@ -1,9 +0,0 @@
-package com.tictactoerest.tictactoegame.parsers;
-
-import java.io.IOException;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.SAXException;
-
-public interface parser {
-    void parse(String path) throws ParserConfigurationException, SAXException, IOException;
-}
